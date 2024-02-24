a08e577d0d1fbd21fa0be0c9a221a2452bc05d7c
Radu <rvatase@gmail.com>
2024-02-24 23:52:16 +0200

    update

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
     output:'export',
 
-    images:{
-        remotePatterns:[
-            {
-            protocol: 'https',
-            hostname: "images.pexels.com"
-            }
-        ]
-    }
 }
 
 module.exports = nextConfig
